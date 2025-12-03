43f40b7a714e75a9317eb690d03c681b47db9b8c
galgoczierik <galgoczi.erik-2023@keri.mako.hu>
2025-12-03 13:36:25 +0100

    kolcsonzes_ellenorzes

--- js/script.js
+++ js/script.js
@@ -24,3 +24,35 @@ let allatszallito = document.querySelector("#allatszallito");
 
 allatszallito.style.backgroundImage = "url('./img/allatszallito_kicsi.jfif')";
 
+
+// Rendelés ellenőrzés
+  let emailInput = document.getElementById("email");
+  let nev = document.getElementById("email");
+  let gomb = document.getElementById("kiberlem");
+  let uzenet = document.getElementById("uzenet");
+  let tel = document.getElementById("telefon");
+  let potkocsi_tipus = document.getElementById("potkocsi_tipus")
+  let igaz = true
+    if (emailInput && tel && potkocsi_tipus && nev && gomb && uzenet)
+        {
+            igaz = true;
+        }
+    else{
+            igaz = false;
+        };
+        
+      if (igaz = true) {
+        gomb.disabled = false;
+      } 
+      else {
+        gomb.disabled = true;
+        uzenet.textContent = "Nem minden adatot töltött ki!";
+        uzenet.style.color = "red";
+      } 
+     
+
+    gomb.addEventListener("click", () => {
+      alert("Köszönjük rendelését!");
+      gomb.disabled = true;
+  });
+
