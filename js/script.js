db60e1fcd4646aa93adf9244017f31d753c57b07
kalladzsino <kalla.dzsino-2023@keri.mako.hu>
2025-12-10 19:54:41 +0100

    navbar elvileg megy mobil nézetben is

--- js/script.js
+++ js/script.js
@@ -1,14 +1,27 @@
 // Navbar & Header
 
-
 fetch("navbar.html")
   .then(response => response.text())
   .then(data => {
-    let el = document.getElementById("jsHeader")
-    if (el) { el.innerHTML = data };
+    let el = document.getElementById("jsHeader");
+    if (el) { 
+        el.innerHTML = data;
+
+        const mobileMenuButton = document.getElementById("mobile-menu-button");
+        const mobileMenu = document.getElementById("mobile-menu");
+        const hamburgerIcon = document.getElementById("hamburger-icon");
+        const closeIcon = document.getElementById("close-icon");
+
+        if(mobileMenuButton){
+          mobileMenuButton.addEventListener("click", () => {
+            mobileMenu.classList.toggle("hidden");
+            hamburgerIcon.classList.toggle("hidden");
+            closeIcon.classList.toggle("hidden");
+          });
+        }
+    }
   })
   .catch(error => console.error("Hiba a menü betöltődésekor!", error));
-
 
 fetch("footer.html")
   .then(response => response.text())
@@ -18,9 +31,9 @@ fetch("footer.html")
   })
   .catch(error => console.error("Hiba a lábléc betöltődésekor!", error));
 
+
 // Kép behúzás
 
 let allatszallito = document.querySelector("#allatszallito");
 
 allatszallito.style.backgroundImage = "url('./img/allatszallito_kicsi.jfif')";
-
